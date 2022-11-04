bc08461775b065e189833af2e989f911284af5ee
danielflorencio <danielflorencio20@gmail.com>
2022-11-04 04:25:08 -0300

    starting the portfolio repository

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+function App() {
+  return (
+    <>
+    <div className='hero-section'>
+      <nav>
+        <ul>
+          <li>Projects</li>
+          <li>Tech Stack</li>
+          <li>My Resume <icon>Download icon</icon></li>
+          <button>Contact me</button>
+        </ul>
+      </nav>
+      <div className='container-inner-column'>
+        <h1 className='main-title'>Hi, i'm Daniel.</h1>
+        <h2>And this is the website's subtitle.</h2>
+      </div>
+      <div className='container-inner-column'>
+
+      </div>
+    </div>
+    <div className='section background-color-dark-grey'>
+
+    </div>
+    </>
+
+  );
+}
+
+export default App;
